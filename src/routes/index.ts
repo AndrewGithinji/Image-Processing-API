dc51cc1fea29fb8475d5ca95058582e8fa68736b
AndrewGithinji <andrewgithinji@hotmail.com>
2023-02-01 10:11:26 +0000

    Updated index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import images from './api/images';
+import images from './api/imagesRoute';
+import listImagesRouter from './api/imagelist';
 
-const routes: express.Router = express.Router();
+const routes = express.Router();
 
-routes.use('/api/images', images);
-
+routes.use('/images', images);
+routes.use('/listImages', listImagesRouter);
 
 export default routes;
